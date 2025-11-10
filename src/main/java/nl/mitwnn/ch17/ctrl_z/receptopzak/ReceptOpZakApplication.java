50bd11657fd3b84f77991c7813c69d6147177a23
pellemeuzelaar <pellemeuzelaar@gmail.com>
2025-11-10 11:35:37 +0100

    first commit (empty spring project)

---
+++ src/main/java/nl/mitwnn/ch17/ctrl_z/receptopzak/ReceptOpZakApplication.java
@@ -0,0 +1,13 @@
+package nl.mitwnn.ch17.ctrl_z.receptopzak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptOpZakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptOpZakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/mitwnn/ch17/ctrl_z/receptopzak/ReceptOpZakApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mitwnn.ch17.ctrl_z.receptopzak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptOpZakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
