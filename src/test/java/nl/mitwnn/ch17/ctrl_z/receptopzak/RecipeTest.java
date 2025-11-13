796e51e02c29841bbcb40692d9c4c4adde3ecd3c
sabienruijgrok <sabienruijgrok@hotmail.com>
2025-11-13 13:39:33 +0100

    made a first test to test the getter in recipe

---
+++ src/test/java/nl/mitwnn/ch17/ctrl_z/receptopzak/RecipeTest.java
@@ -0,0 +1,39 @@
+package nl.mitwnn.ch17.ctrl_z.receptopzak;
+
+
+import nl.mitwnn.ch17.ctrl_z.receptopzak.controller.RecipeController;
+import nl.mitwnn.ch17.ctrl_z.receptopzak.model.Category;
+import nl.mitwnn.ch17.ctrl_z.receptopzak.model.Ingredient;
+import nl.mitwnn.ch17.ctrl_z.receptopzak.model.Recipe;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Sabien Ruijgrok
+ * Test methods from the Recipe Class
+ */
+
+
+public class RecipeTest {
+
+    @Test
+    @DisplayName("test if de getter from recipeName works correct")
+    void testRecipeName() {
+        Recipe recipe = new Recipe();
+
+        recipe.setRecipeName("Patat");
+
+        assertEquals("Friet", recipe.getRecipeName());
+
+    }
+
+}
