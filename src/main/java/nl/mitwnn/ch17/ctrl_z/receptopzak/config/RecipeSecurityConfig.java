e18eb35dcdb8da97c6caea75735e765592e44ee1
sabienruijgrok <sabienruijgrok@hotmail.com>
2025-11-19 10:54:53 +0100

    Enabled and configured security(no users/ui)

---
+++ src/main/java/nl/mitwnn/ch17/ctrl_z/receptopzak/config/RecipeSecurityConfig.java
@@ -0,0 +1,33 @@
+package nl.mitwnn.ch17.ctrl_z.receptopzak.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author Sabien Ruijgrok
+ * Configure the security for the recipe application
+ */
+
+@Configuration
+@EnableWebSecurity
+public class RecipeSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/", "recipe/all").permitAll()
+                        .requestMatchers("/css/**", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .logout((logout) -> logout.logoutSuccessUrl("/"))
+        ;
+
+        return httpSecurity.build();
+    }
+}
